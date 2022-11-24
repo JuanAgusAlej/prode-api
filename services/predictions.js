c3bd55fadd49ff47699e10b6ff0eff1262ee026d
lucianocasini <lucianoo.casini@gmail.com>
2022-11-24 16:33:56 -0300

    Add prediction edit

--- services/predictions.js
+++ services/predictions.js
@@ -2,23 +2,43 @@
 const { User, Match, Prediction } = require('../models');
 
 const createPred = async (goalsA, goalsB, userId, matchId) => {
-  const newPred = new Prediction({
-    goalsA,
-    goalsB,
-    matchId,
-    userId,
-  });
-  try {
+  const prediction = await Prediction.findOne({ matchId, userId });
+  const user = await User.findById(userId);
+  const match = await Match.findById(matchId);
+
+  if (!prediction) {
+    // Create prediction
+    const newPred = new Prediction({
+      goalsA,
+      goalsB,
+      matchId,
+      userId,
+    });
+
     const lastPred = await newPred.save();
-    const user = await User.findById(userId); //  usar service de user
-    const match = await Match.findById(matchId); // usar service de match
+
     user.predictionsId = user.predictionsId.concat(lastPred._id);
     await user.save();
     match.predictionsId = match.predictionsId.concat(lastPred._id);
     await match.save();
     return lastPred;
-  } catch (error) {
-    return error.message;
+  } else {
+    // Update prediction
+    const setPick = () => {
+      if (goalsA > goalsB) return 'WON_A';
+      if (goalsA < goalsB) return 'WON_B';
+      if (goalsA === goalsB) return 'DRAW';
+    };
+    const predictionModified = await Prediction.findByIdAndUpdate(
+      prediction._id,
+      {
+        goalsA,
+        goalsB,
+        pick: setPick(),
+      },
+      { new: true }
+    );
+    return predictionModified;
   }
 };
 
