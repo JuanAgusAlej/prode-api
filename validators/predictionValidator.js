5915c95daf582469fc638d9c31b91c1ce6cd069c
lucianocasini <lucianoo.casini@gmail.com>
2022-11-24 10:44:59 -0300

    Add countdown verification

--- validators/predictionValidator.js
+++ validators/predictionValidator.js
@@ -6,15 +6,24 @@ const validatePrediction = [
   check(['goalsA', 'goalsB'])
     .exists()
     .isInt()
-    .withMessage('Goal prediction must be a number'),
+    .withMessage('Goal prediction must be a number')
+    .isInt({ min: 0 })
+    .withMessage('Goal prediction cannot be a negative number'),
   check('matchId')
     .exists()
     .isMongoId()
     .bail()
     .withMessage('Invalid Match')
     .custom(async (value) => {
-      const matchExists = await Match.findById(value);
-      if (!matchExists) throw new Error("Match doesn't exist");
+      const match = await Match.findById(value);
+      if (!match) throw new Error("Match doesn't exist");
+
+      const actualDate = new Date();
+      const matchDate = match.date;
+      const minutesDiff = Math.abs(
+        Math.round((actualDate - matchDate) / 60000)
+      );
+      if (minutesDiff < 120) throw new Error('The countdown to play is over');
     }),
   (req, res, next) => {
     validateResult(req, res, next);
